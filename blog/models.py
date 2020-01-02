5035169263aece36f03e342ad1850bb999ab1705
Mahesh Prabhu <mp526613@gmail.com>
2020-01-02 14:45:06 +0530

    Finished portfolio site

--- blog/models.py
+++ blog/models.py
@@ -7,4 +7,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     blog_pic=models.ImageField(upload_to='blog_pic/')
 
-    summary = models.CharField(max_length=300)
+    summary = models.CharField(max_length=3000)
+
+
+
+    def __str__(self):
+        return self.title
+
+    def body(self):
+        return self.summary[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
