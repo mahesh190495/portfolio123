ae191c43105fd9bc55010d00a48233f8765e5d76
Mahesh Prabhu <mp526613@gmail.com>
2020-01-03 13:59:23 +0530

    added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
+        'NAME': ' ',
         'USER':'postgres',
-        'PASSWORD':'django123',
+        'PASSWORD':'mahesh908809',
         'HOST':'localhost',
         'PORT':'5432',
     }
@@ -129,3 +129,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
